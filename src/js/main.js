c1848c2e7011a31f58a8cebeca771d07e704c3b3
Anne <annelee3322@gmail.com>
2022-04-28 14:18:06 -0600

    Social icons + updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 $('#emailMe').click(function() {
     $(location).attr('href', 'mailto:annelee3322@gmail.com')
 });
+
+$('#emailAnne').click(function() {
+    $(location).attr('href', 'mailto:annelee3322@gmail.com')
+});
+
+$('#github').click(function () {
+    window.open('https://github.com/dntcarelol');
+});
+
+$('#linkedIn').click(function () {
+    window.open('https://www.linkedin.com/in/annelee22/');
+});
